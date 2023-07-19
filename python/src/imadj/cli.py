98e985c332a3d282fffeaceace6c60c8df176e98
ignatiusm <ignatius.menzies@gmail.com>
2023-07-19 19:03:38 +1200

    refactor(cli): separates out parse_header and adjust_pixels functions

--- python/src/imadj/cli.py
+++ python/src/imadj/cli.py
@@ -11,6 +11,17 @@ app = typer.Typer(
 )
 
 
+class Rotation(str, Enum):
+    left = "left"
+    right = "right"
+    half = "half"
+
+
+class Flip(str, Enum):
+    horizontal = "horizontal"
+    vertical = "vertical"
+
+
 # TODO move to helpers
 def le(byte_str):
     n = 0
@@ -19,14 +30,29 @@ def le(byte_str):
     return n
 
 
-class Rotation(str, Enum):
-    left = "left"
-    right = "right"
-    half = "half"
+# TODO check if these are consistent between bmp fomats
+# add height le(data[22:26])
+def parse_header(data: bytes) -> (bytes, int, int):
+    offset, width = le(data[10:14]), le(data[18:22])
+    spixels = data[offset:]
+    return spixels, offset, width
+
+
+def adjust_bytes(spixels: bytes, width: int) -> bytes:
+    # Iterate in the expected order for *rotated* pixels
+    # look up corresponding *source* pixel, and append to `tpixels`
+    tpixels = []  # TODO: currently only BGR triples - what about alpha?
+    for ty in range(width):  # TODO what should these be for non-squares?
+        for tx in range(width):
+            sy = tx
+            sx = width - ty - 1
+            n = 3 * (sy * width + sx)
+            tpixels.append(spixels[n : n + 3])
+    return tpixels
 
 
 @app.command()
-def rotate(
+def cli(
     infile: Annotated[
         str,
         typer.Argument(
@@ -44,39 +70,26 @@ def rotate(
         ),
     ],
     rotate: Annotated[Rotation, typer.Option(case_sensitive=False)],
+    # flip: Annotated[Flip, typer.Option(case_sensitive=False)],
 ):
     """ """
     # TODO add test to check if infile and outfile are identical and warn
     # (allow interactivity?)
-    if rotate.value == "left":
-        with open(infile, "rb") as f:
-            data = f.read()
 
-        assert data[:2] == b"BM"
-        # TODO check if these are consistent between bmp fomats
-        # add height le(data[22:26])
-        offset, width = le(data[10:14]), le(data[18:22])
+    with open(infile, "rb") as f:
+        data = f.read()
 
-        # Iterate in the expected order for *rotated* pixels
-        # look up corresponding *source* pixel, and append to `tpixels`
-        spixels = data[offset:]
-        tpixels = []  # TODO: currently only BGR triples - what about alpha?
-        for ty in range(width):  # TODO what should these be for non-squares?
-            for tx in range(width):
-                sy = tx
-                sx = width - ty - 1
-                n = 3 * (sy * width + sx)
-                tpixels.append(spixels[n : n + 3])
+    assert data[:2] == b"BM"
 
-        with open(outfile, "wb") as f:
-            f.write(data[:offset])
-            f.write(b"".join(tpixels))
+    spixels, offset, width = parse_header(data)
+    # TODO add rotate.value, flip.value parameters
+    tpixels = adjust_bytes(spixels, width)
 
-        print(f"{infile} rotated left 90 degrees and saved as {outfile}")
-    elif rotate.value == "right":
-        print("rotate right")
-    elif rotate.value == "half":
-        print("rotate 180 degrees")
+    with open(outfile, "wb") as f:
+        f.write(data[:offset])
+        f.write(b"".join(tpixels))
+
+    print(f"{infile} rotated left 90 degrees and saved as {outfile}")
 
 
 if __name__ == "__main__":
