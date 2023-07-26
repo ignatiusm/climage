0cd71e836f341cde9be64d7933da08ee8ccb752c
ignatiusm <ignatius.menzies@gmail.com>
2023-07-26 12:52:04 +1200

    refactor(helpers): renames le for improved readability

--- python/tests/test_helpers.py
+++ python/tests/test_helpers.py
@@ -1,10 +1,10 @@
 # Tests for helper functions
-from imadj.helpers import le
+from imadj.helpers import le_bytes_to_int
 
 
-def test_le():
-    assert le(b"\x00\x10") == 4096
+def test_le_bytes_to_int():
+    assert le_bytes_to_int(b"\x00\x10") == 4096
 
 
-def test_not_be():
-    assert le(b"\x00\x10") != 16
+def test_le_bytes_to_int_is_not_be():
+    assert le_bytes_to_int(b"\x00\x10") != 16
