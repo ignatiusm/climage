d71b9c24a47ea2845a18eae2f0667378ef3e746e
ignatiusm <ignatius.menzies@gmail.com>
2023-07-29 21:07:17 +1200

    feat(cli): adds left and half rotation arguments and tests

--- python/src/imadj/cli.py
+++ python/src/imadj/cli.py
@@ -32,23 +32,36 @@ def parse_header(data: bytes) -> (bytes, int, int):
     return spixels, offset, width
 
 
-def adjust_bytes(spixels: bytes, width: int) -> bytes:
+def adjust_bytes(spixels: bytes, width: int, rotation: str) -> bytes:
     # Iterate in the expected order for *rotated* pixels
     # look up corresponding *source* pixel, and append to `tpixels`
     tpixels = []  # TODO: currently only BGR triples - what about alpha?
     for ty in range(width):  # TODO what should these be for non-squares?
         for tx in range(width):
-            sy = tx
-            sx = width - ty - 1
-            n = 3 * (sy * width + sx)
-            tpixels.append(spixels[n : n + 3])
+            if rotation == "right":
+                sy = tx
+                sx = width - ty - 1
+                n = 3 * (sy * width + sx)
+                tpixels.append(spixels[n : n + 3])
+            elif rotation == "left":
+                sx = ty
+                sy = width - tx - 1
+                n = 3 * (sy * width + sx)
+                tpixels.append(spixels[n : n + 3])
+            elif rotation == "half":
+                sx = width - tx - 1
+                # probably should be height, but we're just dealing with
+                # square images at the moment
+                sy = width - ty - 1
+                n = 3 * (sy * width + sx)
+                tpixels.append(spixels[n : n + 3])
     return tpixels
 
 
-def rotate_right(data):
+def rotate_bmp(data, rotation):
     source_pixels, offset, width = parse_header(data)
     # TODO add rotate.value, flip.value parameters
-    target_pixels = adjust_bytes(source_pixels, width)
+    target_pixels = adjust_bytes(source_pixels, width, rotation)
     return target_pixels, offset
 
 
@@ -80,15 +93,16 @@ def cli(
     with open(infile, "rb") as f:
         data = f.read()
 
+    # Test if file is .bmp
     assert data[:2] == b"BM"
 
-    adjusted_pixels, offset = rotate_right(data)
+    adjusted_pixels, offset = rotate_bmp(data, rotate.value)
 
     with open(outfile, "wb") as f:
         f.write(data[:offset])
         f.write(b"".join(adjusted_pixels))
 
-    print(f"{infile} rotated right 90 degrees and saved as {outfile}")
+    print(f"{infile} rotated {rotate.value} and saved as {outfile}")
 
 
 if __name__ == "__main__":
--- python/tests/test_bmp.py
+++ python/tests/test_bmp.py
@@ -17,11 +17,45 @@ def mock_rotate_right_bmp_image():
     return data
 
 
+@pytest.fixture
+def mock_rotate_left_bmp_image():
+    with open("tests/teapot_left.bmp", "rb") as f:
+        data = f.read()
+    return data
+
+
+@pytest.fixture
+def mock_rotate_half_bmp_image():
+    with open("tests/teapot_half.bmp", "rb") as f:
+        data = f.read()
+    return data
+
+
 def test_rotate_right_is_equal_to_example(
-    mock_original_bmp_image, mock_rotate_right_bmp_image
+    mock_original_bmp_image, mock_rotate_right_bmp_image, rotation="right"
 ):
     assert mock_original_bmp_image[:2] == b"BM"
-    adjusted_pixels, offset = cli.rotate_right(mock_original_bmp_image)
+    adjusted_pixels, offset = cli.rotate_bmp(mock_original_bmp_image, rotation)
     assert (
         mock_original_bmp_image[:offset] + (b"".join(adjusted_pixels))
     ) == mock_rotate_right_bmp_image
+
+
+def test_rotate_left_is_equal_to_example(
+    mock_original_bmp_image, mock_rotate_left_bmp_image, rotation="left"
+):
+    assert mock_original_bmp_image[:2] == b"BM"
+    adjusted_pixels, offset = cli.rotate_bmp(mock_original_bmp_image, rotation)
+    assert (
+        mock_original_bmp_image[:offset] + (b"".join(adjusted_pixels))
+    ) == mock_rotate_left_bmp_image
+
+
+def test_rotate_half_is_equal_to_example(
+    mock_original_bmp_image, mock_rotate_half_bmp_image, rotation="half"
+):
+    assert mock_original_bmp_image[:2] == b"BM"
+    adjusted_pixels, offset = cli.rotate_bmp(mock_original_bmp_image, rotation)
+    assert (
+        mock_original_bmp_image[:offset] + (b"".join(adjusted_pixels))
+    ) == mock_rotate_half_bmp_image
