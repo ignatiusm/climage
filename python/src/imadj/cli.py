44cef3d5974fcd4f375c943c01d601050bda6b80
ignatiusm <ignatius.menzies@gmail.com>
2023-08-02 17:28:00 +1200

    feat(cli): adjusts width and height in header for rectangles
    WIP towards #8

--- python/src/imadj/cli.py
+++ python/src/imadj/cli.py
@@ -5,6 +5,10 @@ from typing_extensions import Annotated
 
 from .helpers import le_bytes_to_int
 
+OFFSET = (10, 14)
+WIDTH = (18, 22)
+HEIGHT = (22, 26)
+
 app = typer.Typer(
     help="""
     imadj is short for IMage ADJust.\n\nimadj is a utility that allows you to
@@ -25,44 +29,70 @@ class Flip(str, Enum):
 
 
 # TODO check if these are consistent between bmp fomats
-# add height le_bytes_to_int(data[22:26])
-def parse_header(data: bytes) -> (bytes, int, int):
-    offset, width = le_bytes_to_int(data[10:14]), le_bytes_to_int(data[18:22])
+def parse_file(data: bytes) -> (bytes, int, int):
+    offset, width, height, bits_per_pixel = (
+        le_bytes_to_int(data[OFFSET[0] : OFFSET[1]]),
+        le_bytes_to_int(data[WIDTH[0] : WIDTH[1]]),
+        le_bytes_to_int(data[HEIGHT[0] : HEIGHT[1]]),
+        le_bytes_to_int(data[28:30]),
+    )
     spixels = data[offset:]
-    return spixels, offset, width
+    return spixels, offset, width, height, bits_per_pixel
 
 
-def adjust_bytes(spixels: bytes, width: int, rotation: str) -> bytes:
+def adjust_header(data: bytes, offset: int, rotation: str) -> bytes:
+    if rotation in "right|left":
+        new_header = (
+            data[: WIDTH[0]]
+            + data[HEIGHT[0] : HEIGHT[1]]
+            + data[WIDTH[0] : WIDTH[1]]
+            + data[HEIGHT[1] : offset]
+        )
+    else:
+        new_header = data[:offset]
+    return new_header
+
+
+def adjust_pixels(
+    spixels: bytes,
+    width: int,
+    height: int,
+    bits_per_pixel: int,
+    rotation: str,
+) -> bytes:
     # Iterate in the expected order for *rotated* pixels
     # look up corresponding *source* pixel, and append to `tpixels`
-    tpixels = []  # TODO: currently only BGR triples - what about alpha?
-    for ty in range(width):  # TODO what should these be for non-squares?
-        for tx in range(width):
+    tpixels = []  # TODO: currently only 24 colour depth
+    for ty in range(width):  # TODO need to adjust bmp header for non-square files
+        for tx in range(height):
             if rotation == "right":
                 sy = tx
                 sx = width - ty - 1
-                n = 3 * (sy * width + sx)
-                tpixels.append(spixels[n : n + 3])
+                n = (bits_per_pixel // 8) * (sy * width + sx)
+                tpixels.append(spixels[n : n + (bits_per_pixel // 8)])
             elif rotation == "left":
                 sx = ty
                 sy = width - tx - 1
-                n = 3 * (sy * width + sx)
-                tpixels.append(spixels[n : n + 3])
+                n = (bits_per_pixel // 8) * (sy * height + sx)
+                tpixels.append(spixels[n : n + (bits_per_pixel // 8)])
             elif rotation == "half":
                 sx = width - tx - 1
                 # probably should be height, but we're just dealing with
                 # square images at the moment
                 sy = width - ty - 1
-                n = 3 * (sy * width + sx)
-                tpixels.append(spixels[n : n + 3])
+                n = (bits_per_pixel // 8) * (sy * width + sx)
+                tpixels.append(spixels[n : n + (bits_per_pixel // 8)])
     return tpixels
 
 
 def rotate_bmp(data, rotation):
-    source_pixels, offset, width = parse_header(data)
+    source_pixels, offset, width, height, bits_per_pixel = parse_file(data)
     # TODO add rotate.value, flip.value parameters
-    target_pixels = adjust_bytes(source_pixels, width, rotation)
-    return target_pixels, offset
+    new_header = adjust_header(data, offset, rotation)
+    target_pixels = adjust_pixels(
+        source_pixels, width, height, bits_per_pixel, rotation
+    )
+    return target_pixels, new_header
 
 
 @app.command()
@@ -96,10 +126,10 @@ def cli(
     # Test if file is .bmp
     assert data[:2] == b"BM"
 
-    adjusted_pixels, offset = rotate_bmp(data, rotate.value)
+    adjusted_pixels, new_header = rotate_bmp(data, rotate.value)
 
     with open(outfile, "wb") as f:
-        f.write(data[:offset])
+        f.write(new_header)
         f.write(b"".join(adjusted_pixels))
 
     print(f"{infile} rotated {rotate.value} and saved as {outfile}")
